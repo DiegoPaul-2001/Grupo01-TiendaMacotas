a1a0c09e488ab897097758e90dbf0e755279561c
DiegoPaul-2001 <diegoquimbit2001@gmail.com>
2021-06-09 10:48:08 -0500

    ver

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     include "config/conexion.php";
 ?>
 <!DOCTYPE html>
-<html lang="es" >
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
