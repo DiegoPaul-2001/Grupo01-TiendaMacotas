b262d167995c45aa1ad113243a15ca88c18cf0a2
DiegoPaul-2001 <diegoquimbit2001@gmail.com>
2021-06-10 23:41:14 -0500

    ingresar mascotas

--- php/mascotas.php
+++ php/mascotas.php
@@ -1,39 +1,18 @@
-<?php
-    include "config/conexion.php";
-    session_start();
-    $usuario = $_SESSION['usuario'];
-
-    require 'config/conexion.php';
-    $query = "select cliNombre as nombre from clientes WHERE cliUsuario = '$usuario'";
-    $query2 = "select cliId as id from clientes WHERE cliUsuario = '$usuario'";
-    $consulta = mysqli_query($db,$query);
-    $consulta2 = mysqli_query($db,$query2);    
-    $array = mysqli_fetch_array($consulta);
-    $array2 = mysqli_fetch_array($consulta2);
-
-    $_SESSION['nombre'] = $array['nombre'];
-    $_SESSION['id'] = $array2['id'];
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
+    <title>Agregar Mascotas</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </head>
-<body background="https://image.freepik.com/vector-gratis/perro-gato-pug-hueso-espinas-pescado-huellas-pata-fondo-vector-patron-transparente-bola_7688-21.jpg">
-   
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <a class="navbar-brand"><img src="img/logo.png" style="height: 60px ; width: 80px;"></a>
         <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -55,54 +34,42 @@
                 <li class="nav-item">
                     <a class="nav-link disabled" href="php/login.php" tabindex="-1" aria-disabled="true">Iniciar Sesion</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="php/mascotas.php" tabindex="-1" aria-disabled="true">Agregar Macotas</a>
-                </li>
             </ul>
         </div>
     </nav>
+    <br><br><br><br><br><br>
 
-
-    <br><br><br><br><br>
     <div class="container">
-        <div class="alert alert-dark">
-            <marquee>
-            <h2><b><-- Bienvenido a !! PELUDOS EC !! <?php echo $array['nombre'];?> --></b></h2>
-            </marquee>
-        </div>
-    </div>
-    
- 
-    <div class="row">
-        <?php
-            $consulta = $db->query("select * from mascota");
-            if($consulta->num_rows > 0){
-                while($row = $consulta->fetch_assoc()){
-        ?>
-        <div class="col-3" >
-            <div class="card">
-                <img class="card-img-top" src="<?php echo$row["rutaFoto"] ?>" alt="img" height="250px;">
-                <div class="card-body">
-                <center>
-                    <h3 class="card-title"><strong>Especie: </strong><?php echo$row["especie"] ?></h3>
-                    <h4 class="card-title"><strong>Raza: </strong><?php echo$row["raza"] ?></h4>
-                    <p class="card-text"><strong>Content: </strong><?php echo$row["detalle"] ?></p>
-                </center>
-                </div>
-                <div class="col-1" >
-                <a class="btn btn-success" href="php/solicitarPedido.php?action=addItem&id=<?php echo $row['id'] ?>">Agregar</a>
-                </div>
-                <br>
+        <center><h2>Agregar mascotas</h2></center>
+        <br><br>
+        <form method="Post" action="">
+            <center><div class="form-group col-5">
+                <label for="">Especie</label><br><input type="text" name="especie" class="form-control"><br>
+                <label for="">Raza</label><br><input type="text" name="raza" class="form-control"><br>
+                <label for="">Detalle</label><br><input type="text" name="detalle" class="form-control"><br>
+                <label for="">Fecha de Nacimiento</label><br><input type="text" name="fechaNacimiento" class="form-control"><br>
+                <label for="">Estado</label><br><input type="text" name="estado" class="form-control"><br>
+                <label for="">Ruta de foto</label><br><input type="text" name="ruta" class="form-control"><br>
+                <label for="">Precio</label><br><input type="text" name="precio" class="form-control"><br>
             </div>
-        </div>
-        <?php
-                }
-            }else{
-                echo"<h1><center><b>No existen resultados</b></center></h1>";
-            }
-        ?>
+            <div>
+                <button class="btn btn-primary" type="button" name="agregar">Agregar</button>
+            </div></center>
+        </form>
     </div>
-
- 
 </body>
 </html>
+<?php
+    if(isset($_POST['agregar'])){
+        $especie = $_POST['especie'];
+        $raza = $_POST['raza'];
+        $detalle = $_POST['detalle'];
+        $fecha = $_POST['fechaNacimiento'];
+        $estado = $_POST['estado'];
+        $ruta = $_POST['ruta'];
+        $precio = $_POST['precio'];
+        $insertar = "INSERT INTO mascota VALUES ('','$especie','$raza','$detalle','$fecha','$estado','$ruta','$precio')";
+        $query = mysqli_query($db, $insertar);
+        
+    }
+?>
